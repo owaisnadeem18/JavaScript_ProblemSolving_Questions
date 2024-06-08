1f6b9ff1f4aebe3f40d4665225a4ef2306c99a98
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-06-08 16:36:15 +0500

    Add files via upload

---
+++ String Methods Interview Questions Practice In JS/Question_01.js
@@ -0,0 +1,34 @@
+// JavaScript Interview Questions Practice
+
+// Write a function reverseString(str) that takes a string as input and returns the string reversed.
+
+// let str = prompt("Dear User ! Please enter a string ");
+
+// function ReverseStr(a) {
+//   return a.split("").reverse().join("");
+// }
+
+// if (isNaN(str)) {
+//   console.log(ReverseStr(str));
+// } else {
+//   alert("Please Enter a valid Input !!!");
+// }
+
+// For Example: if I need to convert the string without using built in methods , then what I have to do is:
+
+// let str = prompt("Dear User ! Please enter a string ");
+
+// function ReverseStr(a) {
+//   let reversed_Str = "";
+//   for (let i = a.length - 1; i >= 0; i--) {
+//     reversed_Str += a[i];
+//   }
+
+//   return reversed_Str;
+// }
+
+// if (isNaN(str)) {
+//   console.log(ReverseStr(str));
+// } else {
+//   alert("Please Enter a valid Input !!!");
+// }
