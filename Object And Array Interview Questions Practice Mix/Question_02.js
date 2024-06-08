9317d2aa0d014826c049114f87b70f8a2a613a55
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-06-08 18:08:27 +0500

    Add files via upload

---
+++ Object And Array Interview Questions Practice Mix/Question_02.js
@@ -0,0 +1,11 @@
+// Question_02
+
+// How we can empty an array , without running a loop , nor we are allowed to reset the array
+
+let arr = [1, 2, 3, 5, 6];
+
+// If I want to empty it , then:
+
+arr.length = 0;
+
+console.log(arr); // array has been empty
