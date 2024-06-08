105a8180c53f8a9fd436b2aa9ea71a5e327c4cf2
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-06-08 18:26:36 +0500

    Add files via upload

---
+++ Mix & Interesting Questions in JavaScript/Question_02.js
@@ -0,0 +1,9 @@
+// Question_02
+
+// you have to duplicate the same array using any technique
+
+function duplicateArr(arr) {
+  return arr.concat(arr);
+}
+
+console.log(duplicateArr([3, 4, 5, 6, 9, 8]));
