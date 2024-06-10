45bbcb6e8fc5d9d19aa7141a86df3102cf84c3bb
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-06-10 16:12:11 +0500

    Add files via upload

---
+++ Mix & Interesting Questions in JavaScript/Question_05.js
@@ -0,0 +1,29 @@
+// Question_05
+
+// Finding the factorial of a number by using a function named as FindFact() using recursion method and technique
+
+// let num = 6;
+
+// let findFact = (n) => {
+//   if (n == 0 || n == 1) {
+//     return 1;
+//   } else {
+//     return n * findFact(n - 1);
+//   }
+// };
+
+// console.log(findFact(num));
+// Finding the factorial of a number by using a function named as FindFact()
+
+// let num = 6;
+
+// let findFact = (n) => {
+//   let fact = 1;
+
+//   for (let i = 1; i <= n; i++) {
+//     fact *= i;
+//   }
+//   return fact;
+// };
+
+// console.log(findFact(num));
