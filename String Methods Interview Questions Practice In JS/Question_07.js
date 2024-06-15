a6961265df801d077ae1bb542e208d61989270d5
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-06-16 00:16:34 +0500

    Add files via upload

---
+++ String Methods Interview Questions Practice In JS/Question_07.js
@@ -0,0 +1,57 @@
+// Reverse a String: Write a function that takes a string as input and returns the string reversed.
+
+// let str = " Hello World ";
+
+// let functionReverse = (string) => {
+//   reversed_Str = "";
+//   for (let i = string.length - 1; i >= 0; i--) {
+//     reversed_Str += string[i];
+//   }
+//   return reversed_Str;
+// };
+
+// console.log(functionReverse(str));
+
+// Check either the string is palindrome or not
+
+// let str = "madam";
+
+// let functionReverse = (string) => {
+//   reversed_Str = "";
+//   for (let i = string.length - 1; i >= 0; i--) {
+//     reversed_Str += string[i];
+//   }
+
+//   if (string == reversed_Str) {
+//     return `The string ${string} is palindrome`;
+//   } else {
+//     return `The string ${string} is not  palindrome`;
+//   }
+// };
+
+// console.log(functionReverse(str));
+
+// Improved code:
+
+// reversing a string
+
+// let str = "Hello";
+
+// function reverseStr(string) {
+//   return string.split("").reverse().join("");
+// }
+
+// console.log(reverseStr(str));
+
+// check either it is palindrome or not:
+
+// let str = "maam";
+
+// function reverseStr(string) {
+//   let reverseString = string.split("").reverse().join("");
+//   return reverseString == str
+//     ? `The string ${string} is palindrome`
+//     : `The string ${string} is not palindrome`;
+// }
+
+// console.log(reverseStr(str));
