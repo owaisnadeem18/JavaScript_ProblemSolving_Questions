be5f254ad74c9ed0bb20e4447f7318e8ac1911e5
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-06-16 00:32:10 +0500

    Add files via upload

---
+++ String Methods Interview Questions Practice In JS/Question_08.js
@@ -0,0 +1,29 @@
+// Question_08
+
+// Longest Word in a Sentence:
+
+// Problem: Write a function that finds the longest word in a given sentence.
+
+// let str = "My name is Owais Nadeem";
+
+// function stringCheck(a) {
+//   let longestStr = "";
+
+//   let splitted_str = a.split(" ");
+
+//   for (let i = 0; i <= splitted_str.length - 1; i++) {
+//     if (splitted_str[i].length > longestStr.length) {
+//       longestStr = splitted_str[i];
+//     }
+//   }
+//   return longestStr;
+
+//   for (let i of splitted_str) {
+//     if (i.length >= longestStr.length) {
+//       longestStr = i;
+//     }
+//   }
+//   return longestStr;
+// }
+
+// console.log(stringCheck(str));
