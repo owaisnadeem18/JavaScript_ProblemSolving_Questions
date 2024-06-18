3ba35faa04acbcdd22608eb720789ad5388a2230
Owais Nadeem <108211200+owaisnadeem18@users.noreply.github.com>
2024-06-18 15:12:03 +0500

    Add files via upload

---
+++ Mix & Interesting Questions in JavaScript/Question_08.js
@@ -0,0 +1,36 @@
+// // find the number of occurences of alphabets in a single string
+
+// let str = "Apple";
+
+// function findOcc(string) {
+//   let occ = {};
+//   let splitted_str = string.split("");
+//   splitted_str.map((a) => {
+//     if (occ.hasOwnProperty(a) == false) {
+//       occ[a] = 1;
+//     } else {
+//       occ[a]++;
+//     }
+//   });
+
+//   return occ;
+// }
+
+// console.log(findOcc(str));
+
+// let str = "Owais";
+
+// function findOcc(string) {
+//   let occurences = {};
+//   let splitted_str = string.split("");
+//   splitted_str.map((item) => {
+//     if (occurences.hasOwnProperty(item) == false) {
+//       occurences[item] = 1;
+//     } else {
+//       occurences[item]++;
+//     }
+//   });
+//   return occurences;
+// }
+
+// console.log(findOcc(str));
